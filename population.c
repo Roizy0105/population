7e109dc01253a6b55a47c34505f1bb9f70ad2c4f
unknown <>
2021-05-14 14:18:25 -0400

    first commit

---
+++ population.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start;
+    do
+    {
+        start = get_int("How many llamas currently exist?: ");
+    }
+    while (start < 9);
+
+
+    // TODO: Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("End amount of llamas?: ");
+    }
+    while (start > end);
+
+
+    // TODO: Calculate number of years until we reach threshold
+    int years = 0;
+    while (start < end)
+    {
+        start += (start / 3) - (start / 4);
+        years++;
+    }
+
+    // TODO: Print number of years
+    printf("Years: %i\n", years);
+}
